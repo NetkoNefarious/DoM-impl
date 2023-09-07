5d0626b980b50c362cdf698cd555e0b498a7582e
Jakov Magazin <89070980+jmagazin-as24@users.noreply.github.com>
2023-09-07 16:49:58 +0200

    Fix playing area test

--- test/model/playingArea.test.ts
+++ test/model/playingArea.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "https://deno.land/std@0.201.0/assert/mod.ts";
 import { Hand } from "../../model/hand.ts";
 import { Card } from "../../model/card.ts";
 import { PlayingArea } from "../../model/playingArea.ts";
-import { isLeft, isRight } from "https://deno.land/x/fun@v2.0.0-alpha.14/either.ts";
+import { isLeft } from "https://deno.land/x/fun@v2.0.0-alpha.14/either.ts";
 
 Deno.test("discard cards from hand", () => {
     // arrange
