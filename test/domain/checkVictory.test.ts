c001b52431b7ed8b7ff1dda04b04b7f9d9980afd
Jakov Magazin <89070980+jmagazin-as24@users.noreply.github.com>
2023-09-13 15:06:27 +0200

    GREEN: for less than 8 stacks, no victory

--- test/domain/checkVictory.test.ts
+++ test/domain/checkVictory.test.ts
@@ -1,6 +1,8 @@
 import { assertStrictEquals } from "https://deno.land/std@0.201.0/assert/assert_strict_equals.ts";
 import { FullStall } from "../util/fullStall.ts";
 import { checkVictory } from "../../domain/checkVictory.ts";
+import { Stall } from "../../model/stall.ts";
+import { assertFalse } from "https://deno.land/std@0.201.0/assert/assert_false.ts";
 
 Deno.test('for 8 stacks, declare victory', () => {
     // arrange
@@ -11,4 +13,15 @@ Deno.test('for 8 stacks, declare victory', () => {
 
     // assert
     assertStrictEquals(result, true)
+})
+
+Deno.test('for less than 8 stacks, no victory', () => {
+    // arrange
+    const stall = new Stall();
+
+    // act
+    const result = checkVictory(stall);
+
+    // assert
+    assertFalse(result)
 })
