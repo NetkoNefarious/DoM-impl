6cf7e265d1b27ec4ccd3314f432b19de9b9636b2
Jakov Magazin <89070980+jmagazin-as24@users.noreply.github.com>
2023-09-13 15:28:42 +0200

    REFACTOR: add stack object mother

--- test/model/stall.test.ts
+++ test/model/stall.test.ts
@@ -1,16 +1,14 @@
 import { assertEquals } from "https://deno.land/std@0.201.0/assert/assert_equals.ts";
-import { Card } from "../../model/card.ts";
-import { Stack } from "../../model/stack.ts";
 import { Stall } from "../../model/stall.ts";
+import { createStackWithCardValues } from "../util/objectMother/stacks.ts";
+import { assertFalse } from "https://deno.land/std@0.201.0/assert/assert_false.ts";
 
 Deno.test("add a stack", () => {
     // arrange
     const stall = new Stall();
 
     // act
-    const isAdded = stall.add(new Stack([
-        new Card("a", "b", "c", false, 1),
-    ]));
+    const isAdded = stall.add(createStackWithCardValues([1]));
 
     // assert
     assertEquals(isAdded, true);
@@ -21,11 +19,9 @@ Deno.test("do not add an invalid stack", () => {
     const stall = new Stall();
 
     // act
-    const isAdded = stall.add(new Stack([
-        new Card("a", "b", "c", false, 2),
-    ]));
+    const isAdded = stall.add(createStackWithCardValues([2]));
 
     // assert
-    assertEquals(isAdded, false);
+    assertFalse(isAdded);
     assertEquals(stall.stacks.length, 0);
 });
---
+++ test/util/objectMother/stacks.ts
@@ -0,0 +1,8 @@
+import { Card } from "../../../model/card.ts";
+import { Stack } from "../../../model/stack.ts";
+
+export function createStackWithCardValues(values: number[]) {
+    const cards = values.map((v) => new Card("a", "b", "c", true, v));
+
+    return new Stack(cards);
+}
