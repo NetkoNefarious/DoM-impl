2dd448eb7a15b211be87561185fb8b4682f45aba
Jakov Magazin <89070980+jmagazin-as24@users.noreply.github.com>
2023-09-13 22:19:01 +0200

    Add hand showing to console

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
+import { Card } from "./model/card.ts";
+import { Hand } from "./model/hand.ts";
+import { showHand } from "./view/showHand.ts";
+
 if (import.meta.main) {
   // Game loop
   while (true) {
-    const input = prompt("Please write a command")
+    const hand = new Hand([
+      new Card('Acorn', "Technique", '', true, 4),
+      new Card('Cookies', "Passive", '', true, 2),
+    ])
+
+    console.log("Hand:");
+    console.log(showHand(hand));
+
+    const input = prompt("Please write a command:")
 
     if (input === "q") {
       break;
     }
+
+    console.log()
   }
 }
